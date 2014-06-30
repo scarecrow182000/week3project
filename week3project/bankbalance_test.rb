b404e19a457617d94130283b5943b7c76b96bb4f
Eric Epperson <eric_d_epperson@hotmail.com>
2014-06-29 23:59:11 -0500

    bankbalance_untested.rb

--- week3project/bankbalance_test.rb
+++ week3project/bankbalance_test.rb
@@ -8,12 +8,17 @@ require './bankbalance'
 class BankBalanceTest < Minitest::Test
 
   # def test_bank_balance_exists
-  #   balance = BankBalance.new
+  #   BankBalance.new
   # end
 
+
+
   def test_starting_account_balance_is_zero
-    assert_equal 0, BankBalance.new.balance
+    assert_equal 0,   BankBalance.new.show_balance
   end
 
-
 end
+
+  # def test_can_deposit
+  #   assert_equal 100, @balance = BankBalance.deposit(100)
+  # end
